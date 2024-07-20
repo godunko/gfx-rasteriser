6565ef116257651081d29d53e2260036600c789f
Vadim Godunko <vgodunko@gmail.com>
2024-07-20 10:14:06 +0400

    Add image generation.

--- testsuite/source/thick_line_1.adb
+++ testsuite/source/thick_line_1.adb
@@ -6,10 +6,17 @@
 
 with GFX.Drawing.Primitive_Rasterizer;
 
+with PPM;
 with Test_Support;
 
 procedure Thick_Line_1 is
 begin
    GFX.Drawing.Primitive_Rasterizer.Rasterize_Line
      ((5.0, 5.0), (15.0, 15.0), 2.0, Test_Support.Print_Span'Access);
+
+   PPM.Initialize (20, 20, Test_Support.Green);
+   GFX.Drawing.Primitive_Rasterizer.Rasterize_Line
+     ((5.0, 5.0), (15.0, 15.0), 2.0, Test_Support.Set_PPM_Span'Access);
+   PPM.Save ("thick_line_1.ppm");
+   PPM.Finalize;
 end Thick_Line_1;
